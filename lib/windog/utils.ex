fcbdd268bdf583d8dbb08c5fdb877428b5297f11
buysake <sksksk.iex@gmail.com>
2023-08-15 02:48:43 +0900

    Implement Utils.filter_odds/4-5

--- lib/windog/utils.ex
+++ lib/windog/utils.ex
@@ -4,4 +4,70 @@ defmodule Windog.Utils do
   def build_race_url(%Structs.RaceContext{} = struct) do
     "https://www.winticket.jp/keirin/#{struct.venue.slug}/racecard/#{struct.cup.id}/#{struct.race.day_index}/#{struct.race.r}"
   end
+
+  #
+  # e.g. filter_odds(race.odds, :sanrentan, ["1"], ["1", "2", "3"], ["1", "2", "3"])
+  #
+
+  def filter_odds(%Structs.OddsCategory{} = odds, :sanrentan, one_line, two_line, three_line) do
+    _filter_odds(odds.sanrentan, false, one_line, two_line, three_line)
+  end
+
+  def filter_odds(%Structs.OddsCategory{} = odds, :sanrenpuku, one_line, two_line, three_line) do
+    _filter_odds(odds.sanrenpuku, true, one_line, two_line, three_line)
+  end
+
+  def filter_odds(%Structs.OddsCategory{} = odds, :nishatan, one_line, two_line) do
+    _filter_odds(odds.nishatan, false, one_line, two_line, [])
+  end
+
+  def filter_odds(%Structs.OddsCategory{} = odds, :nishafuku, one_line, two_line) do
+    _filter_odds(odds.nishafuku, true, one_line, two_line, [])
+  end
+
+  defp _filter_odds(
+         [%Structs.OddsItem{} | _] = odds_items,
+         false = _in_not_particular_order,
+         one_line,
+         two_line,
+         three_line
+       )
+       when is_list(one_line) and is_list(two_line) and is_list(three_line) do
+    odds_items
+    |> Enum.filter(fn odds ->
+      case odds.key do
+        [o, t] ->
+          o in one_line and t in two_line
+
+        [o, t, th] ->
+          o in one_line and t in two_line and th in three_line
+      end
+    end)
+  end
+
+  defp _filter_odds(
+         [%Structs.OddsItem{} | _] = odds_items,
+         true = _in_not_particular_order,
+         one_line,
+         two_line,
+         three_line
+       )
+       when is_list(one_line) and is_list(two_line) and is_list(three_line) do
+    odds_items
+    |> Enum.filter(fn odds ->
+      case odds.key do
+        [o, t] ->
+          (o in one_line and t in two_line) or
+            (t in one_line and o in two_line)
+
+        [o, t, th] ->
+          (o in one_line and t in two_line and th in three_line) or
+            (o in one_line and th in two_line and t in three_line) or
+            (t in one_line and o in two_line and th in three_line) or
+            (t in one_line and th in two_line and o in three_line) or
+            (th in one_line and o in two_line and t in three_line) or
+            (th in one_line and t in two_line and o in three_line)
+      end
+    end)
+  end
 end
--- test/windog/utils_test.exs
+++ test/windog/utils_test.exs
@@ -14,4 +14,101 @@ defmodule Windog.UtilsTest do
     assert url ==
              "https://www.winticket.jp/keirin/#{struct.venue.slug}/racecard/#{struct.cup.id}/#{struct.race.day_index}/#{struct.race.r}"
   end
+
+  test "filter_odds/5 with sanrentan" do
+    sample =
+      "test/samples/race_2023042046_2_3.json"
+      |> File.read!()
+      |> Jason.decode!()
+
+    odds = Windog.Convertor.Race.from_response(sample).odds
+
+    assert 1 == odds |> Windog.Utils.filter_odds(:sanrentan, ["1"], ["2"], ["3"]) |> Enum.count()
+
+    assert 2 ==
+             odds
+             |> Windog.Utils.filter_odds(:sanrentan, ["1"], ["2", "3"], ["2", "3"])
+             |> Enum.count()
+
+    assert 4 ==
+             odds
+             |> Windog.Utils.filter_odds(:sanrentan, ["1", "2"], ["1", "2", "3"], ["1", "2", "3"])
+             |> Enum.count()
+  end
+
+  test "filter_odds/5 with sanrenpuku" do
+    sample =
+      "test/samples/race_2023042046_2_3.json"
+      |> File.read!()
+      |> Jason.decode!()
+
+    odds = Windog.Convertor.Race.from_response(sample).odds
+
+    assert 1 == odds |> Windog.Utils.filter_odds(:sanrenpuku, ["1"], ["2"], ["3"]) |> Enum.count()
+
+    assert 1 ==
+             odds
+             |> Windog.Utils.filter_odds(:sanrenpuku, ["1"], ["2", "3"], ["2", "3"])
+             |> Enum.count()
+
+    assert 1 ==
+             odds
+             |> Windog.Utils.filter_odds(:sanrenpuku, ["1", "2"], ["1", "2", "3"], ["1", "2", "3"])
+             |> Enum.count()
+
+    assert 3 ==
+             odds
+             |> Windog.Utils.filter_odds(:sanrenpuku, ["1"], ["2", "3"], ["1", "2", "3", "4"])
+             |> Enum.count()
+  end
+
+  test "filter_odds/4 with nishatan" do
+    sample =
+      "test/samples/race_2023042046_2_3.json"
+      |> File.read!()
+      |> Jason.decode!()
+
+    odds = Windog.Convertor.Race.from_response(sample).odds
+
+    assert 1 == odds |> Windog.Utils.filter_odds(:nishatan, ["1"], ["2"]) |> Enum.count()
+
+    assert 2 ==
+             odds
+             |> Windog.Utils.filter_odds(:nishatan, ["1"], ["2", "3"])
+             |> Enum.count()
+
+    assert 2 ==
+             odds
+             |> Windog.Utils.filter_odds(:nishatan, ["2", "3"], ["2", "3"])
+             |> Enum.count()
+  end
+
+  test "filter_odds/4 with nishafuku" do
+    sample =
+      "test/samples/race_2023042046_2_3.json"
+      |> File.read!()
+      |> Jason.decode!()
+
+    odds = Windog.Convertor.Race.from_response(sample).odds
+
+    assert 1 ==
+             odds
+             |> Windog.Utils.filter_odds(:nishafuku, ["1"], ["2"])
+             |> Enum.count()
+
+    assert 2 ==
+             odds
+             |> Windog.Utils.filter_odds(:nishafuku, ["1"], ["2", "3"])
+             |> Enum.count()
+
+    assert 1 ==
+             odds
+             |> Windog.Utils.filter_odds(:nishafuku, ["2", "3"], ["2", "3"])
+             |> Enum.count()
+
+    assert 6 ==
+             odds
+             |> Windog.Utils.filter_odds(:nishafuku, ["1"], ["1", "2", "3", "4", "5", "6", "7"])
+             |> Enum.count()
+  end
 end
